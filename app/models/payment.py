30a91d9991dd0cb431849d6176faba7265a507c5
Gautam1610 <gautamcollege1610@gmail.com>
2025-04-05 23:47:19 +0530

    add currency validation

--- app/models/payment.py
+++ app/models/payment.py
@@ -1,15 +1,17 @@
 from sqlalchemy import Column, String, Float, Integer, ForeignKey
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 from app.db.base import Base
+from enum import Enum
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from app.models.customer import Customer
+
 
 class Payment(Base):
     __tablename__ = "payments"
 
     id: Mapped[str] = mapped_column(String, primary_key=True, index=True)
     customer_id: Mapped[str] = mapped_column(ForeignKey("customers.id"))
-    currency: Mapped[str] = mapped_column(String(10))
+    currency: Mapped[str] = mapped_column(String(3))
     amount: Mapped[float] = mapped_column(Float)
     customer: Mapped["Customer"] = relationship(back_populates="payments")
--- app/schemas/payment.py
+++ app/schemas/payment.py
@@ -1,9 +1,16 @@
 from app.schemas.base import BaseModel
+from enum import Enum
 
 
+class Currency(str, Enum):
+    USD = "USD"
+    EUR = "EUR"
+    GBP = "GBP"
+    JPY = "JPY"
+    INR = "INR"
 
 class CreatePayment(BaseModel):
     amount: float
-    currency: str
+    currency: Currency
     customer_id: str
     status: str
